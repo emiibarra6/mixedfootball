cbca9313ad6bfba1ed54a0a2877a288caa11489f
GomezFrannco <frangomez.baires@gmail.com>
2022-09-09 00:31:24 -0300

    [Add] Configuración local agregada

---
+++ config/local.ts
@@ -0,0 +1,14 @@
+require("../src/utils/dotenv");
+
+export default {
+  server: {
+    port: process.env.PORT,
+  },
+  database: {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    driver: process.env.DB_DRIVER,
+    password: process.env.DB_PASSWORD,
+  },
+};
