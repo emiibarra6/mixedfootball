8e3e4415f3e1016160c042dddc77a57451c77711
emiibarra6 <emiibarraa@hotmail.com>
2022-09-14 14:30:18 -0300

    Se agrego a routes la verificacion session JWT

--- src/routes/teams.routes.ts
+++ src/routes/teams.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { getAllTeams } from '../services/teams.services'
-
+import { checkAuth } from '../middleware/authMiddleware'
 const route = Router()
 
 // Enviar todos los equipos.
@@ -8,15 +8,15 @@ route.get('/', (req, res) => {
   const teams = getAllTeams('asd')
 })
 
-route.post('/', (req, res) => {
+route.post('/', checkAuth, (req, res) => {
   res.send('Registramos un equipo')
 })
 
-route.patch('/:id', (req, res) => {
+route.patch('/:id', checkAuth, (req, res) => {
   res.send('Editamos un equipo')
 })
 
-route.delete('/:id', (req, res) => {
+route.delete('/:id', checkAuth, (req, res) => {
   res.send('Borramos un equipo')
 })
 
